5b0f75301b48d412abdbb4e4cb7a00148a0fc6b8
Jack Seigerman <133878087+jackSeigerman@users.noreply.github.com>
2023-11-16 17:07:54 -0600

    Dont worry about this guys I totally didnt break it

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Scene mainMenu;
+    [SerializeField] private GameObject ray;
+    [SerializeField] private GameObject button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene("mainMenu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ray.GetComponent<Rigidbody>().c
+    }
+}
