bea3cd23b3e43de6d58a4fc00eadfd7fe70ed0ab
Jack Seigerman <133878087+jackSeigerman@users.noreply.github.com>
2023-11-17 14:54:39 -0600

    Added menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,21 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEditor;
+using UnityEngine.InputSystem;
 public class MainMenu : MonoBehaviour
 {
-    [SerializeField] private Scene mainMenu;
-    [SerializeField] private GameObject ray;
-    [SerializeField] private GameObject button;
+    private GameObject XRRig;
+    private XRControls XRControls;
     // Start is called before the first frame update
     void Start()
     {
-        SceneManager.LoadScene("mainMenu");
+        XRRig = XRRig_Behaviour.Instance.gameObject;
+        XRControls = XRRig_Behaviour.Instance.XRControls;
+
+
+        XRControls.Character.TriggerLeft.started += useLeft;
+        XRControls.Character.TriggerRight.started += useRight;
+        Ray ray = new Ray(transform.position, transform.forward);
     }
 
-    // Update is called once per frame
+    private void useLeft(InputAction.CallbackContext context)
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hitData;
+        if (Physics.Raycast(ray, out hitData))
+
+        {
+            Debug.Log(hitData.transform.name);
+            if(hitData.transform.name.Equals("Exit"))
+            {
+                if (EditorApplication.isPlaying)
+                {
+                    EditorApplication.isPlaying = false;
+                }
+                else
+                {
+                    Application.Quit();
+                }
+            }
+        }
+        
+    }
+    private void useRight(InputAction.CallbackContext context)
+    {
+        //if (itemComponent.GetInfluenceHand() == null || itemComponent.GetInfluenceHand().GetComponent<Hand>().GetSide() != Hand.HandType.Right) return;
+        //Debug.Log("takingingPhoto");
+        //takePhoto();
+    }
+    
+   /* void FireRay()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hitData;
+        Physics.Raycast(ray, out hitData);
+        string tag = hitData.collider.tag;
+    }*/
     void Update()
     {
-        //if ray.GetComponent<Rigidbody>().cS
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hitData;
+        if (Physics.Raycast(ray, out hitData))
+        
+        {
+            Debug.Log(hitData.transform.name);
+        }
     }
 }
