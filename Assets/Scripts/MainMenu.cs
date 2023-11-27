7754b97c635f03d821e13e3789297831bc056cd5
Jack Seigerman <133878087+jackSeigerman@users.noreply.github.com>
2023-11-27 00:52:52 -0600

    idk

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,7 +8,12 @@ public class MainMenu : MonoBehaviour
 {
     private GameObject XRRig;
     private XRControls XRControls;
-    // Start is called before the first frame update
+
+    [SerializeField] private GameObject start;
+    [SerializeField] private GameObject settings;
+    [SerializeField] private GameObject credits;
+    [SerializeField] private GameObject exit;
+
     void Start()
     {
         XRRig = XRRig_Behaviour.Instance.gameObject;
@@ -63,6 +68,24 @@ public class MainMenu : MonoBehaviour
 
     void Update()
     {
+        start.transform.GetComponent<MeshRenderer>().material.color = Color.white;
+        settings.transform.GetComponent<MeshRenderer>().material.color = Color.white;
+        credits.transform.GetComponent<MeshRenderer>().material.color = Color.white;
+        exit.transform.GetComponent<MeshRenderer>().material.color = Color.white;
 
+
+
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hitData;
+        if (Physics.Raycast(ray, out hitData))
+        {
+            if ((hitData.transform.name.Equals("Exit")) || (hitData.transform.name.Equals("Credits")) || (hitData.transform.name.Equals("Settings")) || (hitData.transform.name.Equals("Play")))
+            {
+                hitData.transform.GetComponent<MeshRenderer>().material.color = Color.red;
+            }
+           
+
+        }
+        
     }
 }
